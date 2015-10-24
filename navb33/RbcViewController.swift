6cf7436556d8f9ab364110e51c3f427748421f29
Steven Cassidy <steven@mumsnet.com>
2015-10-24 13:20:56 -0400

    Add a dateFromJSON function

--- navb33/RbcViewController.swift
+++ navb33/RbcViewController.swift
@@ -19,5 +19,11 @@ class RbcViewController: UIViewController {
         self.presentViewController(alert, animated: true, completion: nil)
     }
 
+    func dateFromJson(dateString:String) -> NSDate? {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+        return dateFormatter.dateFromString(dateString)
+    }
 
 }
